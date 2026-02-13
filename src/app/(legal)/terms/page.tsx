ffd2c882ab3da4c3eea5032a226f2936a70eb3da
Ege Uysal <hi@egeuysal.com>
2026-02-12 22:18:50 -0600

    chore(docs): create docsa

---
+++ src/app/(legal)/terms/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="h-screen w-full flex items-center justify-center bg-black">
+      <h1 className="text-white opacity-30 text-lg font-mono">Coming soon</h1>
+    </div>
+  );
+}
